52774a11e7264d1eec1fe3ec89be624a939c8ee6
milanovicmilos <2002mm9.mm@gmail.com>
2023-11-01 19:06:37 +0100

    feat: added profile fragment

--- app/src/main/java/com/example/bookingappteam17/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/bookingappteam17/fragments/ProfileFragment.java
@@ -1,6 +1,32 @@
 package com.example.bookingappteam17.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import com.example.bookingappteam17.R;
+
 public class ProfileFragment extends Fragment {
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
 }
