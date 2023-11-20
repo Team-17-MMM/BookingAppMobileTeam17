a04162acc9100820d47a06396c3b18f161dde433
MilanJovkic <milanjovkic@gmail.com>
2023-11-20 14:05:10 +0100

    added screen orientation

--- app/src/main/java/com/example/bookingappteam17/activities/LoginActivity.java
+++ app/src/main/java/com/example/bookingappteam17/activities/LoginActivity.java
@@ -1,15 +1,25 @@
 package com.example.bookingappteam17.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import com.example.bookingappteam17.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private boolean isPermissions = true;
+    private String [] permissions = {
+            Manifest.permission.INTERNET,
+    };
+    private static final int REQUEST_PERMISSIONS = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +36,32 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
             startActivity(intent);
         });
+        onRequestPermission();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case REQUEST_PERMISSIONS:
+                for(int i = 0; i < permissions.length; i++) {
+                    Log.i("ShopApp", "permission " + permissions[i] + " " + grantResults[i]);
+                    if(grantResults[i] == PackageManager.PERMISSION_DENIED){
+                        isPermissions = false;
+                    }
+                }
+                break;
+        }
+
+        if (!isPermissions) {
+            Log.e("ShopApp", "Error: no permission");
+            finishAndRemoveTask();
+        }
+
+    }
+
+    private void onRequestPermission(){
+        Log.i("ShopApp", "onRequestPermission");
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_PERMISSIONS);
     }
 }
