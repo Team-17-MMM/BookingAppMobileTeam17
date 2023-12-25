1cd621e8bf952c26ce31cf241da2c9238dfb70c8
MilanJovkic <milanjovkic@gmail.com>
2023-12-25 09:13:57 +0100

    added accommodation registration layout

---
+++ app/src/main/java/com/example/bookingappteam17/activities/RegisterAccommodationActivity.java
@@ -0,0 +1,17 @@
+package com.example.bookingappteam17.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bookingappteam17.R;
+
+public class RegisterAccommodationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accommodation_reservation);
+
+    }
+}
